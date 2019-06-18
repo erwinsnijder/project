7868877b7ebb448407c3dfc69c6a00bda470c3ef
erwin snijder <erwinsnijder_4@hotmail.com>
2019-06-18 10:01:50 +0200

    update ProjectsController

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// get  /projects        (index)
+// get  /projects/create (create)
+// get  /projects/1      (show)
+// post /projects        (store)
+// get  /projects/1/edit (edit)
+// path /projects/1      (update)
+// path /projects/1      (destroy)
 
-Route::get('/projects', 'ProjectsController@index');
-Route::post('/projects', 'ProjectsController@store');
-Route::get('/projects/create', 'ProjectsController@create');
+route::resource('projects', 'ProjectsController');
+// Route::get('/projects', 'ProjectsController@index');
+// Route::get('/projects/create', 'ProjectsController@create');
+// route::get('/projects/{project}', 'ProjectsController@show');
+// Route::post('/projects', 'ProjectsController@store');
+// route::get('/projects/{project}/edit', 'ProjectsController@edit');
+// route::patch('/projects/{project}', 'ProjectsController@update');
+// route::delete('/projects/{project}', 'ProjectsController@destroy');
