947b6e6dec8220d845a9ce083df55590f4fed727
erwin snijder <erwinsnijder_4@hotmail.com>
2019-06-18 16:38:24 +0200

    cobtroller

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -8,18 +8,25 @@ class ProjectsController extends Controller
 {
     public function index()
     {
-        $projects = \App\Project::all();
+        $projects = Project::all();
 
         return view('projects.index', compact('projects'));
     }
+
+    public function show(project $project)
+{
+    return view('projects.show', compact('project'));
+}
+
     public function create()
     {
         return view('projects.create');
 }
 
-public function show(project $project)
+public function store()
 {
-    return view('projects.show', compact('project'));
+    project::create(request(['title', 'description']));
+        return redirect('/projects');
 }
 
 public function edit(project $project) 
@@ -28,18 +35,10 @@ public function edit(project $project)
 }
 
 public function update(project $project)
-{
-    $project->title = request('title');
-    $project->description = request('description');
-    $project->save();
+{   
+    $project->update(request(['title', 'description']));
     return redirect('/projects');
 }
-
-public function store()
-{
-    project::create(request(['title', 'description']));
-        return redirect('/projects');
-}  
     
 public function destroy(project $project)
 {
